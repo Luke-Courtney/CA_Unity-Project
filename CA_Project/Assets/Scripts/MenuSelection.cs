7222ae5e572ce91bc4668dd4af75c3c454f67885
Luke Courtney <k00267879@student.tus.ie>
2023-10-24 13:10:59 +0100

    [24/10/23] Added menu functionality and audio

--- CA_Project/Assets/Scripts/MenuSelection.cs
+++ CA_Project/Assets/Scripts/MenuSelection.cs
@@ -2,6 +2,7 @@ using System.Collections;
 using System.Collections.Generic;
 using UnityEditor;
 using UnityEngine;
+using UnityEngine.SceneManagement;
 
 public class MenuSelection : MonoBehaviour
 {
@@ -16,11 +17,22 @@ public class MenuSelection : MonoBehaviour
     //Z Position
     private int initalSelection = 10;
 
+    //Sounds
+    private AudioSource selectSource;
+    public AudioClip selectSound;
+    public AudioClip enterSound;
+
+    //Enter timer
+    //Gives time for sound to play before continuing
+    private float elapsedTime = 0.0f;
+
     // Start is called before the first frame update
     void Start()
     {
         selection = initalSelection;
         selectionIndicator = GameObject.Find("Selector");
+
+        selectSource = GameObject.Find("Select_Sound").GetComponent<AudioSource>();
     }
 
     // Update is called once per frame
@@ -48,6 +60,8 @@ public class MenuSelection : MonoBehaviour
             {
                 selection = 0;
             }
+
+            selectSource.PlayOneShot(selectSound, 0.7f);
         }
         else if (Input.GetKeyDown(KeyCode.S))
         {
@@ -58,6 +72,37 @@ public class MenuSelection : MonoBehaviour
             else if (selection == 0)
             {
                 selection = -selectionSpacing;
+            }
+
+            selectSource.PlayOneShot(selectSound, 0.7f);
+        }
+
+
+        if(Input.GetKeyDown(KeyCode.Return))
+        {
+            selectSource.PlayOneShot(enterSound, 0.7f);
+
+            while(elapsedTime < 100.0f)
+            {
+                elapsedTime += Time.deltaTime;
+                Debug.Log("Elapsed:" + elapsedTime.ToString());
+            }
+
+            elapsedTime = 0.0f;
+
+            switch (selection)
+            {
+                case 10:
+                    SceneManager.LoadScene("Game");
+                    break;
+
+                case 0:
+                    Debug.Log("Scores");
+                    break;
+
+                case -10:
+                    Application.Quit();
+                    break;
             }
         }
     }
